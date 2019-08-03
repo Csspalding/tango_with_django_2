9bac3db4aa705937f5b57bb8d3bb3fc24de35079
Cassie Spalding <2140148s@student.gla.ac.uk>
2019-08-03 16:26:00 +0100

    corrected templates dir error, templates link now works

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 #index view is the home page for the project
 #view handles responce to request from client
 
 #notice capital letter for name in HTML is not the same as variable name in urls.py
 def index(request):
-  return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
-  # this worked click and paste link "http://127.0.0.1:8000/rango/about"
+  #return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
 
+  #construct a dictionary to pass to the template engin as its context.
+  #Note the key boldmessage is the dame as {{boldmessage}} in the template
+  context_dict= {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+  #return a rendered responce to send to the client
+  #make use of shortcut function 
+  return render(request, 'rango/index.html', context=context_dict)
+ 
 def about(request):
   return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#added this templated variable to join base dir variable with newly created templates directories
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,8 +59,9 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[TEMPLATE_DIR,],
         'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -119,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
