e467a163d00e182b4ab39247e2415454f5b796ff
Cassie Spalding <2140148s@student.gla.ac.uk>
2019-08-06 11:11:19 +0100

    category/<category.slug>  category.html view model added

--- populate_rango.py
+++ populate_rango.py
@@ -25,11 +25,12 @@ def populate():
 #categories dictionary of dictionaries lists above 
 	cats = {"Python": {"pages": python_pages, 'views':128, 'likes':64}, 
 			"Django": {"pages": django_pages,'views':64, 'likes':32 }, 
-			"Other Frameworks": {"pages": other_pages,'views':32, 'likes':16} }
+			"Other Frameworks": {"pages": other_pages,'views':132, 'likes':16} }
+      
 #loop through cats dictionary add each category then add all the associated pages for that category
 
 	for cat, cat_data in cats.items():
-		c = add_cat(cat, cat_data ["views"], cat_data ["likes"])
+		c = add_cat(cat, cat_data ["views"], cat_data ["likes"] )
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"])
 
--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 # Create your views here.
 #index view is the home page for the project
 #view handles responce to request from client
 
 #notice capital letter for name in HTML is not the same as variable name in urls.py
 def index(request):
-  #return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+  #To test return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+
+  #query db for a list of ALL categories stored, ordered by likes in -decending order limit to 5
+  category_list = Category.objects.order_by('-likes')[:5]
+
 
   #construct a dictionary to pass to the template engin as its context.
   #Note the key boldmessage is the dame as {{boldmessage}} in the template
-  context_dict= {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+  context_dict = {}
+  context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
+  context_dict['categories'] = category_list
   #return a rendered responce to send to the client
   #make use of shortcut function 
   return render(request, 'rango/index.html', context=context_dict)
@@ -20,3 +28,26 @@ def about(request):
   context_dict={'boldmessage': 'This tutorial has been put together by Cassie'}
   return render(request, 'rango/about.html', context=context_dict)
 #now mapp your Url to the view
+
+def show_category(request, category_name_slug):#stores encoded category name
+  context_dict = {} #initialise dictionary
+  #extract data from the model, add it to context_dic, handle exception render reponce. 
+  try:
+    # get () returns DoesNotExist // try/catch  handles exception if no categories exist
+    # or returns a model instance
+    category = Category.objects.get(slug=category_name_slug)
+
+    #filter()  will return a list of page objects or an empty list
+    pages = Page.objects.filter(category=category)
+
+    #Saves results from filter() to template context under name pages
+    context_dict['pages'] = pages
+    #adds the category object from db to the context dictionary
+    # which is used to verify it exists in the template 
+    context_dict['category']= category
+  except Category.DoesNotExist:
+    #set context_dict to None as none found, DoesNotExist exception is displayed
+    context_dict['pages'] = None 
+    context_dict['category']= None 
+  #render the request as a responce to the client
+  return render(request, 'rango/category.html', context=context_dict)
